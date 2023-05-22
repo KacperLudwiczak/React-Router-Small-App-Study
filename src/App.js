a89819ad0d347e1aa264ed49f43f9cbfdb58d32b
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-05-22 23:47:18 +0200

    Error

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
