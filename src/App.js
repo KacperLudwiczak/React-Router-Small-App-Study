646e1e8931e120a528fe9196a8adf9a93c177024
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-05-22 23:03:11 +0200

    Create Browser Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/Home";
+
+const router = createBrowserRouter([{ path: "/", element: <HomePage /> }, {}]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
