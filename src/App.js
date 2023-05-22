b1dfcc4915940e3891bdd2f1ce871e81423fbf17
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-05-22 23:10:09 +0200

    Products Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
 
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }, {}]);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
